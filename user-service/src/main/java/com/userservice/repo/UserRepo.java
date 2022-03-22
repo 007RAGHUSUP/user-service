e283b300f04b9e37b4c90faf7bc3484fbf61775f
Naresh-cha <94688086+Naresh-cha@users.noreply.github.com>
2022-03-22 15:46:59 +0530

    Delete user-service directory

--- user-service/src/main/java/com/userservice/repo/UserRepo.java
+++
@@ -1,6 +0,0 @@
-package com.userservice.repo;
-import com.userservice.model.UserModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepo extends MongoRepository<UserModel, String> {
-}
--- user-service/src/main/java/com/userservice/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.userservice.service;
-
-import com.userservice.model.UserModel;
-
-public interface UserService  {
-    UserModel createPost(UserModel user);
-}
